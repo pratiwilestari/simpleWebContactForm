045a785eeb22ada5d8001a6d0937f038c1c8bca9
Anastasia Pratiwi <pratiwilestari95@gmail.com>
2021-07-14 20:26:01 +0700

    update 14072021

--- swcf/controllers/index.py
+++ swcf/controllers/index.py
@@ -1,5 +1,6 @@
+from flask.wrappers import Request
 from swcf import app
-from flask import render_template
+from flask import render_template, redirect, request, url_for
 from swcf.dao.indexDAO import *
 
 @app.route("/", methods=['GET'])
@@ -7,3 +8,15 @@ def index():
     coba = selectOne()
     print(coba)
     return render_template("layout.html")
+
+@app.route("/sendPost", methods=['POST'])
+def sendPost():
+    print('masuk sini')
+    name = request.form['name']
+    email = request.form['email']
+    issue = request.form['issue']
+    # content = request.form['content']
+    print(name, email, issue)
+    hInsert = insertPost(name, email, issue, 'content')
+    if hInsert['flag'] == 'T':
+        return render_template("layout.html")
--- swcf/dao/indexDAO.py
+++ swcf/dao/indexDAO.py
@@ -14,3 +14,19 @@ def selectOne():
     finally:
         if(conn):
             conn.close()
+
+def insertPost(name, email, issue, content):
+    conn = dbConnect()
+    try:
+        cursor = conn.cursor()
+        query = '''INSERT INTO post VALUES(
+                        SELECT COALESCE(MAX(id), 1) FROM post,
+                        %s, %s, %s, %s); '''
+        cursor.execute(query, (name, email, issue, content, ))
+        data = rows_to_dict(cursor)
+        return responseJSON('T', 'sukses select', None) 
+    except Exception as err:
+        return responseJSON('F', str(err), None)
+    finally:
+        if(conn):
+            conn.close()
